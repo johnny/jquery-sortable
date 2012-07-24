5060ba455717bb817988450f0963a02877488e14
Jonas von Andrian <jvadev@gmail.com>
2012-07-24 22:32:21 +0200

    Update pages. See main branch for changes

--- js/examples/nested_with_switch.js
+++ js/examples/nested_with_switch.js
@@ -1,6 +1,20 @@
 $(function () {
+  var oldContainer
   $("ol.nested_with_switch").sortable({
-    group: 'nested'
+    group: 'nested',
+    afterMove: function (placeholder, container) {
+      if(oldContainer != container){
+        if(oldContainer)
+          oldContainer.el.removeClass("active")
+        container.el.addClass("active")
+        
+        oldContainer = container
+      }
+    },
+    onDrop: function (item, container, _super) {
+      container.el.removeClass("active")
+      _super(item)
+    }
   })
   
   $(".switch-container").on("click", ".switch", function  (e) {
