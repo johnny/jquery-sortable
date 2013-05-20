cd541dc4a00fee3da848b429c79b981436d44885
Jonas von Andrian <jvadev@gmail.com>
2013-05-20 11:10:45 +0200

    Release 0.9.10

--- source/js/jquery-sortable.js
+++ source/js/jquery-sortable.js
@@ -1,5 +1,5 @@
 /* ===================================================
- *  jquery-sortable.js v0.9.9
+ *  jquery-sortable.js v0.9.10
  *  http://johnny.github.com/jquery-sortable/
  * ===================================================
  *  Copyright (c) 2012 Jonas von Andrian
